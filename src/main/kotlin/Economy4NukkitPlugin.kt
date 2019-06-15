c67bbabef300a05d73fc37a7148385454e99ab27
uramnoil <uramnoil@outlook.jp>
2019-06-15 14:09:11 +0900

    Economy4NukkitPluginクラスの作成

--- src/main/kotlin/Economy4NukkitPlugin.kt
+++ src/main/kotlin/Economy4NukkitPlugin.kt
@@ -3,7 +3,13 @@ package com.uramnoil.economy4nukkit
 
 import cn.nukkit.plugin.PluginBase
 
+/**
+ * Economy4Nukkitの本体
+ */
 class Economy4NukkitPlugin: PluginBase() {
+    /**
+     * 起動時
+     */
     override fun onEnable() {
         server.logger.info("Fuck")
     }
